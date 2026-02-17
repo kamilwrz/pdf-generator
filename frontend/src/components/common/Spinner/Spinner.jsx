12e09dc2b5bcd28b05e6129efa724ce5a5c250b3
kamwrz <kwrzochalski@gmail.com>
2026-02-17 19:31:33 +0100

    debuging

--- frontend/src/components/common/Spinner/Spinner.jsx
+++ frontend/src/components/common/Spinner/Spinner.jsx
@@ -2,5 +2,5 @@ import {CircleLoader} from "react-spinners";
 
 export default function Spinner({loading = true}){
     if(!loading) return null;
-   return <CircleLoader loading={true} size={150} color="#60a5fa" cssOverride={{position: "absolute", left:"50%", top: "50%", transform: "translate(-50% , -50%)"}}/>
+   return <CircleLoader loading={true} size={150} color="#60a5fa" cssOverride={{position: "absolute", left:"50%", top: "40%", transform: "translate(-50% , -50%)", zIndex: "2000"}}/>
 }
