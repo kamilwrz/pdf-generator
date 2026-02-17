fbea8399663b34436e61eff52deecd5cbcc9807e
kamwrz <kwrzochalski@gmail.com>
2026-02-17 13:23:49 +0100

    origins

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
-origins = ["https://pdf-generator-react.onrender.com"]
+origins = [
+    "http://localhost:5173",
+    "https://pdf-generator-react.onrender.com"
+]
 PDF_UPLOAD_DIR = Path() / "static/generated"
 IMAGES_UPLOAD_DIR = Path() / "uploads"
 
