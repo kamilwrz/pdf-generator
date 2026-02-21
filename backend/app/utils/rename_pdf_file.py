42f1af18491d6264d6fe267c1ebf0a9a85554ce8
kamwrz <kwrzochalski@gmail.com>
2026-02-21 04:16:05 +0100

    file path update pdf debug

--- backend/app/utils/rename_pdf_file.py
+++ backend/app/utils/rename_pdf_file.py
@@ -5,14 +5,14 @@ def rename_pdf_file(pdf:object, title: str):
     pdf_file_path = str(pdf.file_path)
 
     print(pdf_file_path, "RENAME")
-    new_file_path = pdf_file_path.split("\\")
+    new_file_path = pdf_file_path.split("/")
     new_file_path[-1] = title
 
     print(new_file_path, "NEW FIEL APTH AFTER TITLE CHANGE")
-    os.rename(pdf_file_path, "\\".join(new_file_path))
+    os.rename(pdf_file_path, "/".join(new_file_path))
 
     pdf.title = title
-    pdf.file_path = "\\".join(new_file_path)
+    pdf.file_path = "/".join(new_file_path)
 
     print(pdf.file_path, "ROW FILE PATH AFTER CHANGING")
 
