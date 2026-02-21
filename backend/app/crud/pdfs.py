97df334e83c2c5c3a21395cbe74ae65c34eb9535
kamwrz <kwrzochalski@gmail.com>
2026-02-21 09:36:46 +0100

    deleted to pdf - bug

--- backend/app/crud/pdfs.py
+++ backend/app/crud/pdfs.py
@@ -86,10 +86,10 @@ def update_pdf_elements(db:Session, elements:list, existing_elements:dict, pdf_i
             )
             db.add(pdf_elements)
 
-        if element.element_id in existing_elements and element.extra_properties.deleted == True:
+        if element.element_id in existing_elements and element.deleted == True:
             db.query(PdfElements).filter(PdfElements.element_id == element.element_id).delete()
             db.flush()
-            
+
         else:
             existing_row = existing_elements[element.element_id]
             existing_row.left = element.left
