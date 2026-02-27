effe99bf25f26e43be967c77d57b27bc2f5f9244
kamwrz <kwrzochalski@gmail.com>
2026-02-28 00:17:19 +0100

    Download PDF

--- frontend/src/utils/download.js
+++ frontend/src/utils/download.js
@@ -1,5 +1,5 @@
 export async function download(url){
-    const response = await fetch(url, {credentials: "include"});
+    const response = await fetch(url, {headers:{"Authorization": `Bearer ${localStorage.getItem("token")}`}, credentials: "include"});
     const blob = await response.blob();
     const blobUrl = URL.createObjectURL(blob)
     console.log(blobUrl);
